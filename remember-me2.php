9b740752a9ea529f4a27b63a7c7210102aa18c15
Laura Sage <laura@yourbusybee.com>
2015-10-06 18:57:24 -0400

    Create remember-me2.php

---
+++ remember-me2.php
@@ -0,0 +1,10 @@
+// Set Remember Met to checked
+function login_checked_remember_me() {
+add_filter( 'login_footer', 'rememberme_checked' );
+}
+add_action( 'init', 'login_checked_remember_me' );
+
+function rememberme_checked() {
+echo "<script>document.getElementById('rememberme').checked = true;</script>";
+}
+// END set Remember Me
